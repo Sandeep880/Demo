7d6244eb32c8689284232afaf323eb0c93e945cf
Sandeep880 <79862064+Sandeep880@users.noreply.github.com>
2025-03-31 17:37:37 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ package java8;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Learning Jenkins");
+		System.out.println("Learning Jenkins with youtube tutorials");
 
 	}
 
